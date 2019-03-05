1acc42f6be75f8da0df1cf3d1ec5e20df11efaef
Martin Nestorov <mnestorov@protonmail.com>
2019-03-05 23:00:12 +0200

    Adds command input and output

--- basic-shell.c
+++ basic-shell.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
+#define COMMAND_LENGHT 100
+#define PARAMETER_LENGHT 10
+
+char* get_command(char* line);
+
+int main(int argc, char* argv[])
 {
-    char* input;
+    char line[100];
     while (1)
     {
-	printf("%s", "[basic-shell@root] ~$ ");
-	scanf("%s", input);
+        printf("[%s@basic-shell] ~$ ", getenv("USER"));
+	get_command(line);
+	printf("%s\n", line);
     }
     return 0;
+}
+
+char* get_command(char* line)
+{
+    if (fgets(line, sizeof(line), stdin) == NULL) return "";
+    // Remove trailing neommand character
+    if (line[strlen(line) - 1] == '\n') {
+	line[strlen(line) - 1] = '\0';
+    }
+    return line;
 }
