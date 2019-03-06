1aa927a523497170f4a27dc149bbf5232dd9e15b
Martin Nestorov <mnestorov@protonmail.com>
2019-03-06 12:57:10 +0200

    Adds line parsing of command

--- basic-shell.c
+++ basic-shell.c
@@ -5,26 +5,44 @@
 #define COMMAND_LENGHT 100
 #define PARAMETER_LENGHT 10
 
-char* get_command(char* line);
+void get_command(char* comm);
+void parse(char* comm, char** params);
+void execute(char* comm, char** params);
 
 int main(int argc, char* argv[])
 {
-    char line[100];
+    char comm[COMMAND_LENGHT + 1];
+    char* params[PARAMETER_LENGHT + 1];
     while (1)
     {
         printf("[%s@basic-shell] ~$ ", getenv("USER"));
-	get_command(line);
-	printf("%s\n", line);
+	get_command(comm);
+	parse(comm, params);
+	execute(comm, params);
+	printf("%s\n", comm);
     }
     return 0;
 }
 
-char* get_command(char* line)
+void get_command(char* comm)
 {
-    if (fgets(line, sizeof(line), stdin) == NULL) return "";
+    if (fgets(comm, sizeof(comm), stdin) == NULL) comm = NULL;
     // Remove trailing neommand character
-    if (line[strlen(line) - 1] == '\n') {
-	line[strlen(line) - 1] = '\0';
+    if (comm[strlen(comm) - 1] == '\n') {
+	comm[strlen(comm) - 1] = '\0';
     }
-    return line;
+}
+
+void parse(char* comm, char** params)
+{
+    for (int i = 0; i < PARAMETER_LENGHT; i++)
+    {
+	params[i] = strsep(&comm, " ");
+	if (params[i] == NULL) break;
+    }
+}
+
+void execute(char* comm, char** params)
+{
+
 }
