aec2b66335905929d80041dabe2aa2f81dcd058f
Martin Nestorov <mnestorov@protonmail.com>
2019-03-09 17:51:07 +0200

    Adds builtin commands

--- basic-shell.c
+++ basic-shell.c
@@ -9,13 +9,19 @@
 
 #define COMMAND_LENGHT 1024
 #define PARAMETER_LENGHT 64
+#define BUILTIN_COMMS 2
 
 char* get_line();
 char** parse(char* comm);
 int execute(char** params);
+bool is_builtin(char* comm);
+int exec_builtin(char** params);
+int exec_std(char** params);
 void push_to_bg(void);
 
 bool bg = false;
+
+char* builtin_commands[] = { "cd", "exit" };
 
 int main(void)
 {
@@ -106,6 +112,53 @@ int execute(char** params)
 	return 0;
     }
 
+    if (is_builtin(params[0]))
+    {
+	return exec_builtin(params);
+    }
+    else
+    {
+	return exec_std(params);
+    }
+}
+
+bool is_builtin(char* comm)
+{
+    for (int i = 0; i < BUILTIN_COMMS; i++)
+    {
+	if (strcmp(builtin_commands[i], comm) == 0)
+	{
+	    return true;
+	}
+    }
+    return false;
+}
+
+int exec_builtin(char** params)
+{
+    if (strcmp(params[0], "cd") == 0)
+    {
+	if (params[1] == NULL)
+	{
+	    fprintf(stderr, "basic-shell: %s: %s\n", params[0], strerror(errno));
+	    return 1;
+	}
+	if (chdir(params[1]) != 0)
+	{
+	    fprintf(stderr, "basic-shell: %s: %s\n", params[0], strerror(errno));
+	    return 1;
+	}
+    }
+    else if (strcmp(params[0], "exit") == 0)
+    {
+	printf("%s", "exiting...");
+	exit(0);
+    }
+    return 1;
+}
+
+int exec_std(char** params)
+{
     pid_t pid = fork();
     if (pid < 0)
     {
